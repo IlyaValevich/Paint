68c84534d61808a311cfe20bfb0d868bd83b6b0f
cpnk <ilya.valevich@gmail.com>
2019-07-24 15:11:20 +0300

    Initial Commit

---
+++ Paint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Paint
+//
+//  Created by Ilya on 7/24/19.
+//  Copyright © 2019 Ilya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Paint/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Paint
+//
+//  Created by Ilya on 7/24/19.
+//  Copyright © 2019 Ilya. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
